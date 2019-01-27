bef8199bfcbbc4dc47c1f2e357b6829ce162130b
James Blachly <james.blachly@gmail.com>
2019-01-27 18:27:20 -0500

    Add cigar and tagvalue to the package

--- source/dhtslib/package.d
+++ source/dhtslib/package.d
@@ -1,7 +1,9 @@
 module dhtslib;
 
 public import dhtslib.bgzf;
+public import dhtslib.cigar;
 public import dhtslib.faidx;
 public import dhtslib.sam;
 public import dhtslib.tabix;
+public import dhtslib.tagvalue;
 public import dhtslib.vcf;
