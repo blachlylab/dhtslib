c1444b92e29994392c35c94e3dab60b21eedec63
James Blachly <james.blachly@gmail.com>
2019-01-27 18:23:07 -0500

    cigar: code reformat and documentation

--- source/dhtslib/cigar.d
+++ source/dhtslib/cigar.d
@@ -1,35 +1,44 @@
+/**
+    This module simplifies working with CIGAR strings/ops in SAM/BAM/CRAM format.
+*/
 module dhtslib.cigar;
 
 import std.stdio;
-import std.bitmanip:bitfields;
-import std.array:join;
-import std.algorithm:map;
-import std.algorithm.iteration:each;
-import std.conv:to;
-import std.range:array;
+import std.bitmanip : bitfields;
+import std.array : join;
+import std.algorithm : map;
+import std.algorithm.iteration : each;
+import std.conv : to;
+import std.range : array;
 
 import dhtslib.htslib.hts_log;
 
-
-/**
-Represents a cigar string
-*/
+/// Represents a CIGAR string
+/// https://samtools.github.io/hts-specs/SAMv1.pdf §1.4.6
 struct Cigar
 {
+    /// array of distinct CIGAR ops 
     CigarOp[] ops;
 
-    this(uint * cigar, int length)
+    /// Construct Cigar from raw data
+    this(uint* cigar, int length)
     {
-        cigar[0..length].each!(x=>ops~=CigarOp(x));
-    }
-    this(CigarOp[] ops){
-        this.ops=ops;
+        cigar[0 .. length].each!(x => ops ~= CigarOp(x));
     }
 
+    /// Construct Cigar from an array of CIGAR ops
+    this(CigarOp[] ops)
+    {
+        this.ops = ops;
+    }
+
+    /// Format Cigar struct as CIGAR string in accordance with SAM spec
     string toString()
     {
-        return ops.map!(x=>x.length.to!string~CIGAR_STR[x.op]).array.join;
+        return ops.map!(x => x.length.to!string ~ CIGAR_STR[x.op]).array.join;
     }
+
+    /// return the alignment length expressed by this Cigar
     int alignedLength()
     {
         int length = 0;
@@ -37,16 +46,16 @@ struct Cigar
         {
             switch (op.op)
             {
-                default:
+            default:
                 {
                     break;
                 }
 
-                case Ops.MATCH:
-                case Ops.EQUAL:
-                case Ops.DIFF:
-                case Ops.DEL:
-                case Ops.INS:
+            case Ops.MATCH:
+            case Ops.EQUAL:
+            case Ops.DIFF:
+            case Ops.DEL:
+            case Ops.INS:
                 {
                     length += op.length;
                     break;
@@ -57,30 +66,30 @@ struct Cigar
     }
 }
 
-/**
-Represents a cigar operation
-*/
+/// Represents a distinct cigar operation
 union CigarOp
 {
+    /// raw opcode
     uint raw;
 
-    mixin(bitfields!(
-    //lower 4 bits store op
-    Ops,"op",4,
-    //higher 28 bits store length
-    uint,"length",28
-    ));
+    mixin(bitfields!(//lower 4 bits store op
+            Ops, "op", 4,//higher 28 bits store length
+            uint, "length", 28));
 
+    /// construct Op from raw opcode
     this(uint raw)
     {
-        this.raw=raw;
+        this.raw = raw;
     }
-    this(uint len,Ops op)
+
+    /// construct Op from an operator and operand (length)
+    this(uint len, Ops op)
     {
-        this.op=op;
-        this.length=len;
+        this.op = op;
+        this.length = len;
     }
 }
+
 /**
 Represents all ops
 #define BAM_CIGAR_STR   "MIDNSHP=XB"
@@ -94,61 +103,70 @@ Represents all ops
 #define BAM_CEQUAL      7
 #define BAM_CDIFF       8
 #define BAM_CBACK       9
-
 */
-string CIGAR_STR="MIDNSHP=XB";
-enum Ops {
-    MATCH    =  0,
-    INS      =  1,
-    DEL      =  2,
-    REF_SKIP =  3,
-    SOFT_CLIP=  4,
-    HARD_CLIP=  5,
-    PAD      =  6,
-    EQUAL    =  7,
-    DIFF     =  8,
-    BACK     =  9
+string CIGAR_STR = "MIDNSHP=XB";
+/// ditto
+enum Ops
+{
+    MATCH = 0,
+    INS = 1,
+    DEL = 2,
+    REF_SKIP = 3,
+    SOFT_CLIP = 4,
+    HARD_CLIP = 5,
+    PAD = 6,
+    EQUAL = 7,
+    DIFF = 8,
+    BACK = 9
 }
 
-
-unittest{
+unittest
+{
     writeln();
     import dhtslib.sam;
     import dhtslib.htslib.hts_log;
+
     hts_set_log_level(htsLogLevel.HTS_LOG_TRACE);
-    hts_log_info(__FUNCTION__,"Testing cigar");
-    hts_log_info(__FUNCTION__,"Loading test file");
-    auto bam=SAMFile("htslib/test/range.bam",0);
-    auto readrange=bam["CHROMOSOME_I",914];
-    hts_log_info(__FUNCTION__,"Getting read 1");
-    auto read=readrange.front();
+    hts_log_info(__FUNCTION__, "Testing cigar");
+    hts_log_info(__FUNCTION__, "Loading test file");
+    auto bam = SAMFile("htslib/test/range.bam", 0);
+    auto readrange = bam["CHROMOSOME_I", 914];
+    hts_log_info(__FUNCTION__, "Getting read 1");
+    auto read = readrange.front();
     writeln(read.queryName);
-    hts_log_info(__FUNCTION__,"Cigar:"~read.cigar.toString());
-    assert(read.cigar.toString()=="78M1D22M");
+    hts_log_info(__FUNCTION__, "Cigar:" ~ read.cigar.toString());
+    assert(read.cigar.toString() == "78M1D22M");
 }
 
-Cigar cigarFromString(string cigar) {
+/// return Cigar struct for a given CIGAR string (e.g. from SAM line)
+Cigar cigarFromString(string cigar)
+{
     import std.regex;
-    return Cigar(match(cigar, regex(`(\d+)([A-Z=])`, "g"))
-    .map!(m => CigarOp(m[1].to!uint, m[2].to!char.charToOp))
-    .array);
+
+    return Cigar(match(cigar, regex(`(\d+)([A-Z=])`, "g")).map!(m => CigarOp(m[1].to!uint,
+            m[2].to!char.charToOp)).array);
 }
 
-Ops charToOp(char c){
-    foreach(i,o;CIGAR_STR){
-        if(c==o){
-            return cast(Ops)i;
+/// Convert single char representing a CIGAR Op to Op union
+Ops charToOp(char c)
+{
+    foreach (i, o; CIGAR_STR)
+    {
+        if (c == o)
+        {
+            return cast(Ops) i;
         }
     }
-    return cast(Ops)9;
+    return cast(Ops) 9;
 }
 
-unittest{
+unittest
+{
     writeln();
-    hts_log_info(__FUNCTION__,"Testing string2cigar");
-    hts_log_info(__FUNCTION__,"Loading test file");
-    string c="130M2D40M";
-    auto cig=cigarFromString(c);
-    hts_log_info(__FUNCTION__,"Cigar:"~cig.toString());
-    assert(cig.toString()==c);
+    hts_log_info(__FUNCTION__, "Testing string2cigar");
+    hts_log_info(__FUNCTION__, "Loading test file");
+    string c = "130M2D40M";
+    auto cig = cigarFromString(c);
+    hts_log_info(__FUNCTION__, "Cigar:" ~ cig.toString());
+    assert(cig.toString() == c);
 }
