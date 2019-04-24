24bf2b87bd4e0c2f7964ab9447e4663cdb55b160
Charles Gregory <charles.gregory@osumc.edu>
2019-04-24 15:40:44 -0400

    formatting and comments for fastq.d

--- source/dhtslib/fastq.d
+++ source/dhtslib/fastq.d
@@ -1,34 +1,58 @@
 module dhtslib.fastq;
 import dhtslib.bgzf;
 
-struct FastqRecord{
+/*
+ *  Represents a complete fastq read record.
+ */ 
+struct FastqRecord
+{
     string id;
     string sequence;
     string extra;
     string qscores;
 }
 
-struct FastqFile{
+/*
+ *  Parses a bgzipped, gzipped, or plain-text fastq file 
+ *  into a range of FastqRecords.
+ */ 
+struct FastqFile
+{
     BGZFile f;
     FastqRecord rec;
-    this(string fn){
+
+    this(string fn)
+    {
         f=BGZFile(fn);
         popFront;
     }
-    FastqRecord front(){
+
+    FastqRecord front()
+    {
         return rec;
     }
-    void popFront(){
+
+    void popFront()
+    {
+        //get id line
         rec.id = f.front;
         f.popFront;
+
+        //get seq line
         rec.sequence = f.front;
         f.popFront;
+
+        //get extra line
         rec.extra = f.front;
         f.popFront;
+
+        //get qscore line
         rec.qscores = f.front;
         f.popFront;
     }
-    bool empty(){
+
+    bool empty()
+    {
         return f.empty;
     }
 
