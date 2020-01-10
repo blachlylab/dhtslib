08a0d1e1940a52ad4ddfa041547f1686ee327ef7
James Blachly <james.blachly@gmail.com>
2020-01-10 17:21:34 -0500

    1.10 update: htslib.thread_pool

--- source/htslib/thread_pool.d
+++ source/htslib/thread_pool.d
@@ -4,8 +4,8 @@ extern(C):
 // @file htslib/thread_pool.h
 // Thread pool for multi-threading applications.
 /*
-    Copyright (c) 2013-2017 Genome Research Ltd.
-
+    Copyright (c) 2013-2017, 2019 Genome Research Ltd.
+    
     Author: James Bonfield <jkb@sanger.ac.uk>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -46,6 +46,8 @@ DEALINGS IN THE SOFTWARE.  */
  * To see example usage, please look at the #ifdef TEST_MAIN code in
  * thread_pool.c.
  */
+
+import htslib.hts_defs;
 
 /*-----------------------------------------------------------------------------
  * Opaque data types.
@@ -94,6 +96,9 @@ struct hts_tpool_result;
  *
  * Returns pool pointer on success;
  *         NULL on failure
+ *
+ * The hts_tpool struct returned by a successful call should be freed
+ * via hts_tpool_destroy() when it is no longer needed.
  */
 hts_tpool *hts_tpool_init(int n);
 
@@ -104,27 +109,84 @@ hts_tpool *hts_tpool_init(int n);
 int hts_tpool_size(hts_tpool *p);
 
 
+/// Add an item to the work pool.
 /**
- * Adds an item to the work pool.
- *
- * FIXME: permit q to be NULL, indicating a global/default pool held by
- * the thread pool itself?  This pool would be for jobs that have no
- * output, so fire and forget only with..
- *
- * Returns 0 on success
+ * @param p     Thread pool
+ * @param q     Process queue
+ * @param func  Function run by the thread pool
+ * @param arg   Data for use by func()
+ * @return 0 on success
  *        -1 on failure
  */
-
 // FIXME: should this drop the hts_tpool*p argument? It's just q->p
-//int hts_tpool_dispatch(hts_tpool *p, hts_tpool_process *q,
-//                       void *(*func)(void *arg), void *arg);
 int hts_tpool_dispatch(hts_tpool *p, hts_tpool_process *q,
                         void* function(void* arg), void *arg);
-/// ditto
-//int hts_tpool_dispatch2(hts_tpool *p, hts_tpool_process *q,
-//                        void *(*func)(void *arg), void *arg, int nonblock);
+
+/// Add an item to the work pool, with nonblocking option.
+/**
+ * @param p         Thread pool
+ * @param q         Process queue
+ * @param func      Function run by the thread pool
+ * @param arg       Data for use by func()
+ * @param nonblock  Non-blocking flag (see description)
+ * @return 0 on success
+ *        -1 on failure
+ *
+ * The @p nonblock parameter can take one of the following values:
+ *      0 => block if input queue is full
+ *     +1 => don't block if input queue is full, but do not add task
+ *     -1 => add task regardless of whether queue is full (over-size)
+ *
+ * If @p nonblock is +1 and the queue is full, -1 will be returned and
+ * `errno` is set to `EAGAIN`.
+ */
 int hts_tpool_dispatch2(hts_tpool *p, hts_tpool_process *q,
                         void* function(void* arg), void *arg, int nonblock);
+
+/// Add an item to the work pool, with nonblocking and cleanup callbacks.
+/**
+ * @param p               Thread pool
+ * @param q               Process queue
+ * @param exec_func       Function run by the thread pool
+ * @param arg             Data for use by func()
+ * @param job_cleanup     Callback to clean up when discarding jobs
+ * @param result_cleanup  Callback to clean up when discarding result data
+ * @param nonblock        Non-blocking flag (see description)
+ * @return 0 on success
+ *        -1 on failure
+ *
+ * The @p nonblock parameter can take one of the following values:
+ *      0 => block if input queue is full
+ *     +1 => don't block if input queue is full, but do not add task
+ *     -1 => add task regardless of whether queue is full (over-size)
+ *
+ * If @p nonblock is +1 and the queue is full, -1 will be returned and
+ * `errno` is set to `EAGAIN`.
+ *
+ * The job_cleanup() and result_cleanup() callbacks are used when discarding
+ * data from a queue, for example when calling hts_tpool_process_reset()
+ * or hts_tpool_process_destroy().
+ *
+ * If not NULL, job_cleanup() will be called for each pending job with the
+ * value of @p arg that was set for that job.  This can be used to free
+ * any data associated with @p arg, and also @p arg itself.
+ *
+ * Similarly, result_cleanup() can be used to free any results left by
+ * jobs that had started before hts_tpool_process_reset() was called.
+ * The argument passed to result_cleanup() is the pointer that would
+ * have been returned by calling hts_tpool_result_data() on the result
+ * when pulled from the queue.
+ *
+ * job_cleanup() and result_cleanup() are only called when discarding jobs.
+ * For jobs that are processed normally, it is the resposibility of
+ * exec_func() and / or consumers of any results to do any cleaning up
+ * necessary.
+ */
+int hts_tpool_dispatch3(hts_tpool *p, hts_tpool_process *q,
+                        void* function(void* arg) exec_func, void *arg,
+                        void function(void* arg) job_cleanup,
+                        void function(void* data) result_cleanup,
+                        int nonblock);
 
 /**
  * Wakes up a single thread stuck in dispatch and make it return with
@@ -220,6 +282,9 @@ void *hts_tpool_result_data(hts_tpool_result *r);
  *
  * Results hts_tpool_process pointer on success;
  *         NULL on failure
+ *
+ * The hts_tpool_process struct returned by a successful call should be freed
+ * via hts_tpool_process_destroy() when it is no longer needed.
  */
 hts_tpool_process *hts_tpool_process_init(hts_tpool *p, int qsize, int in_only);
 
@@ -228,17 +293,6 @@ hts_tpool_process *hts_tpool_process_init(hts_tpool *p, int qsize, int in_only);
  * Must be called before the thread pool is destroyed.
  */
 void hts_tpool_process_destroy(hts_tpool_process *q);
-
-/*
- * Flushes the thread pool, but doesn't exit. This simply drains the
- * process-queue and ensures all worker threads have finished their current
- * task if associated with this process.
- *
- * Returns 0 on success;
- *        -1 on failure
- */
-// Duplicate prototype with slightly different doc
-//int hts_tpool_process_flush(hts_tpool_process *q);
 
 /**
  * Returns true if there are no items in the process results queue and
