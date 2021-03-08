e0e78d63bfc0499a2e2519c6c4a48bc9695829cc
James Blachly <james.blachly@gmail.com>
2021-03-08 18:02:44 -0500

    Integration test update
    - README
    - samreader: reworked, removed cruft
    - samreader: added asserts
    - samreader: Final assert fails due to issue #59

---
+++ README.txt
@@ -0,0 +1,1 @@
+Integration tests -- i.e., those testing multiple aspects of the library API, potentially interconnected; and those accessing external files -- go here
--- test/samreader.d
+++ test/samreader.d
@@ -4,29 +4,13 @@ import std.stdio;
 import std.string;
 
 import dhtslib.sam;
-import dhtslib.htslib.sam;
-import dhtslib.htslib.hts_log;
+import htslib.sam;
+import htslib.hts_log;
 
 int main()
 {
-    debug(dhtslib_debug)
-    {
-        writeln("enabling debug logging");
-        hts_set_log_level(htsLogLevel.HTS_LOG_TRACE);
-
-        // Test log levels
-        hts_log_info(__FUNCTION__, "Testing log levels: expect I(nfo) D(ebug) W(arning) E(rror) T(race)");
-        hts_log_debug(__FUNCTION__, "Test debug");
-        hts_log_warning(__FUNCTION__, "Test warning");
-        hts_log_error(__FUNCTION__, "Test error");
-        hts_log_trace(__FUNCTION__, "Test trace (after which will reset to Debug level)");
-
-        hts_set_log_level(htsLogLevel.HTS_LOG_DEBUG);
-    }
-
-
-    //auto sf = SAMFile("/Users/james/Documents/Development/blachlylab/funmap/wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam");
-    auto sf = SAMFile("/Users/james/Documents/Development/blachlylab/funmap/ENCFF399AWI.bam");
+    hts_set_log_level(htsLogLevel.HTS_LOG_INFO);
+    auto sf = SAMFile("./htslib/test/range.bam");
 
     writeln("Basic SAM/BAM/CRAM data\n-----------------------");
     writefln("Input file: %s", sf.filename);
@@ -34,25 +18,26 @@ int main()
     writefln("Length of contig 0: %d", sf.target_len(0));
     writefln("Length of all targets: %s", sf.target_lens);
     writefln("Target names array: %s", sf.target_names);
-    writefln("Lookup by name: \"chr5\" is the %d'th target (0-indexed).", sf.target_id("chr5"));
+    writefln("Lookup by name: \"CHROMOSOME_I\" is the %d'th target (0-indexed).", sf.target_id("CHROMOSOME_I"));
 
-    writeln("Now testing default class constructor");
+    hts_log_info(__FUNCTION__, "Default class constructor");
     for(int j; j<100_000; j++)
     {
         auto x = new SAMRecord();
     }
 
-    writeln("Query c raw htslib");
+    hts_log_info(__FUNCTION__, "Query via htslib C interface");
     {
         int j = 0;
-        auto fn = toStringz("/Users/james/Documents/Development/blachlylab/funmap/ENCFF399AWI.bam");
-        import dhtslib.htslib.hts;
+        auto fn = toStringz("./htslib/test/range.bam");
+
+        import htslib.hts;
         auto fp = hts_open(fn, cast(immutable(char)*)"r".ptr);
         auto idx= sam_index_load(fp, fn);
         bam1_t *b = bam_init1();
         hts_itr_t *iter;
         int r;
-        if ((iter = sam_itr_queryi(idx, 0, 1_000_000, 2_000_000)) == null) {
+        if ((iter = sam_itr_queryi(idx, 0, 1_000, 2_000)) == null) {
             hts_log_error(__FUNCTION__, "Failed to parse region");
             return 1;
         }
@@ -63,40 +48,44 @@ int main()
         }
 
         writefln("Processed %d records with raw iter", j);
+        assert(j == 14);
 
         hts_itr_destroy(iter);
         bam_destroy1(b);
         hts_close(fp);
     }
 
-    writeln("Testing query with D wrapper");
-    int j;
-    //auto qr = sf.query("chr1:1000000-2000000");
-    auto qr = sf.query(0, 1_000_000, 2_000_000);
+    hts_log_info(__FUNCTION__, "Query via D wrapper (SAMFile.query())");
+    {
+        int j;
+        //auto qr = sf.query("chr1:1000000-2000000");
+        //auto qr = sf.query(0, 1_000_000, 2_000_000);
+        auto qr = sf.query(0, 1_000, 2_000);
+        foreach(r; qr) {
+            j++;
+        }
+        writefln("%d records", j);
+        assert(j == 14);
+    }
+
+    hts_log_info(__FUNCTION__, "Test query with zero expected results");
+    int j = 0;
+    auto qr = sf.query(0, 1, 2);
     foreach(r; qr) {
         j++;
     }
     writefln("%d records", j);
+    assert(j == 0);
 
-    writeln("Testing query with expected no results");
-    j = 0;
-    qr = sf.query(0, 1, 2);
-    foreach(r; qr) {
-        j++;
-    }
-    writefln("%d records", j);
-
-    writeln("Now testing AllRecordsRange");
+    hts_log_info(__FUNCTION__, "Test AllRecordsRange iterator");
     int i;
     auto x = sf.all_records;
     foreach(r; x) {
         i++;
-        //writeln(i);
-        //writeln(fromStringz(r.sequence));
+        //writeln(r.queryName);
     }
     writefln("%d records", i);
+    assert(i == 112);
 
-    writeln("SAMFile going out of scope?");
- 
     return 0;
 }
