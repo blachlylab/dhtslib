9e822c61fbf2d648464abfc2db04d31254c39c44
Charles Gregory <charles.gregory940@topper.wku.edu>
2021-06-09 20:34:03 -0400

    added some comments

--- source/dhtslib/recordwriter.d
+++ source/dhtslib/recordwriter.d
@@ -3,22 +3,30 @@ module dhtslib.recordwriter;
 import std.stdio : File;
 import std.range : ElementType;
 
-
+/**
+* Abstraction for writing string-based records to a file.
+* Intended for use with BedRecord, GFF(2|3)Record.
+*/
 struct RecordWriter(RecType)
 if(__traits(hasMember, RecType, "toString"))
 {
-    File f;
 
+    File f; /// File writer
+
+    /// ctor with filename and header
     this(string fn, string header="")
     {
+        /// open file and write header if any
         this.f = File(fn, "w");
         if(header != "") this.f.writeln(header);
     }
 
+    /// Write record to file
     void write(RecType rec){
         this.f.writeln(rec.toString);
     }
 
+    /// Write a range of records to file
     void writeRecords(Range)(Range range)
     if(is(ElementType!Range == RecType))
     {
