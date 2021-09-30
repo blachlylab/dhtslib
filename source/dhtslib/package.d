e6f809ac1e28612f8894324eb5fe5ca178ebf498
Charles Gregory <charles.gregory940@topper.wku.edu>
2021-09-30 13:08:28 -0400

    added util and memory to package.d

--- source/dhtslib/package.d
+++ source/dhtslib/package.d
@@ -10,3 +10,5 @@ public import dhtslib.gff;
 public import dhtslib.bed;
 public import dhtslib.coordinates;
 public import dhtslib.recordwriter;
+public import dhtslib.memory;
+public import dhtslib.util;
