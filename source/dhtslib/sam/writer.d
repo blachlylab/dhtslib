76f5a32f166b568ec622052a6a11e2dd3343b4bd
charlesgregory <charles.gregory940@topper.wku.edu>
2022-08-29 12:36:11 -0400

    converted SAMWriter over to using dhtslib.file

--- source/dhtslib/sam/writer.d
+++ source/dhtslib/sam/writer.d
@@ -17,6 +17,7 @@ import dhtslib.sam.header;
 import dhtslib.sam : parseSam;
 import dhtslib.memory;
 import dhtslib.util;
+import dhtslib.file;
 
 
 /// SAM/BAM/CRAM on-disk format.
@@ -26,6 +27,8 @@ enum SAMWriterTypes
     BAM,
     UBAM,
     SAM,
+    GZ_SAM,
+    BGZF_SAM,
     CRAM,
     DEDUCE
 }
@@ -33,22 +36,11 @@ enum SAMWriterTypes
 /// Encapsulates a SAM/BAM/CRAM, but as write-only
 struct SAMWriter
 {
-    /// filename; as usable from D
-    string filename;
-
-    /// filename \0-terminated C string; reference needed to avoid GC reaping result of toStringz when ctor goes out of scope
-    private const(char)* fn;
-
-    /// htsFile
-    private HtsFile fp;
-
-    /// hFILE if required
-    private hFILE* f;
+    /// HtslibFile
+    HtslibFile file;
 
     /// header struct
     SAMHeader header;
-
-    private kstring_t line;
 
     /// disallow copying
     @disable this(this);
@@ -62,79 +54,58 @@ struct SAMWriter
                         0  use no extra threads
                         >1 add indicated number of threads (to a default of 1)
     */
-    this(T)(T f,SAMHeader header, SAMWriterTypes t=SAMWriterTypes.DEDUCE,int extra_threads = -1)
+    this(T)(T f,SAMHeader header, SAMWriterTypes t=SAMWriterTypes.DEDUCE,int threads = -1)
     if (is(T == string) || is(T == File))
     {
         import std.parallelism : totalCPUs;
-        char[] mode;
-        if(t == SAMWriterTypes.BAM) mode=['w','b','\0'];
-        else if(t == SAMWriterTypes.UBAM) mode=['w','b','0','\0'];
-        else if(t == SAMWriterTypes.SAM) mode=['w','\0'];
-        else if(t == SAMWriterTypes.CRAM) mode=['w','c','\0'];
+        HtslibFileWriteMode mode;
+        if(t == SAMWriterTypes.BAM) mode = HtslibFileWriteMode.Bam;
+        else if(t == SAMWriterTypes.UBAM) mode = HtslibFileWriteMode.UncompressedBam;
+        else if(t == SAMWriterTypes.SAM) mode = HtslibFileWriteMode.Sam;
+        else if(t == SAMWriterTypes.GZ_SAM) mode = HtslibFileWriteMode.GzippedSam;
+        else if(t == SAMWriterTypes.BGZF_SAM) mode = HtslibFileWriteMode.BgzippedSam;
+        else if(t == SAMWriterTypes.CRAM) mode = HtslibFileWriteMode.Cram;
         // open file
         static if (is(T == string))
         {
             if(t == SAMWriterTypes.DEDUCE){
                 import std.path:extension;
                 auto ext=extension(f);
-                if(ext==".bam") mode=['w','b','\0'];
-                else if(ext==".sam") mode=['w','\0'];
-                else if(ext==".cram") mode=['w','c','\0'];
+                if(ext==".bam") mode = HtslibFileWriteMode.Bam;
+                else if(ext==".sam") mode = HtslibFileWriteMode.Sam;
+                else if(ext==".cram") mode = HtslibFileWriteMode.Cram;
                 else {
                     hts_log_error(__FUNCTION__,"extension "~ext~" not valid");
                     throw new Exception("DEDUCE SAMWriterType used with non-valid extension");
                 }
             }
-            this.filename = f;
-            this.fn = toStringz(f);
-            this.fp = hts_open(this.fn, mode.ptr);
         }
-        else static if (is(T == File))
-        {
-            assert(t!=SAMWriterTypes.DEDUCE);
-            this.filename = f.name();
-            this.fn = toStringz(f.name);
-            this.f = hdopen(f.fileno, cast(immutable(char)*) "w");
-            this.fp = hts_hopen(this.f, this.fn, mode.ptr);
-        }
-        else assert(0);
-
-        if (extra_threads == -1)
-        {
-            if ( totalCPUs > 1)
-            {
-                hts_log_info(__FUNCTION__,
-                        format("%d CPU cores detected; enabling multithreading", totalCPUs));
-                // hts_set_threads adds N _EXTRA_ threads, so totalCPUs - 1 seemed reasonable,
-                // but overcomitting by 1 thread (i.e., passing totalCPUs) buys an extra 3% on my 2-core 2013 Mac
-                hts_set_threads(this.fp, totalCPUs);
-            }
-        }
-        else if (extra_threads > 0)
-        {
-            if ((extra_threads + 1) > totalCPUs)
-                hts_log_warning(__FUNCTION__, "More threads requested than CPU cores detected");
-            hts_set_threads(this.fp, extra_threads);
-        }
-        else if (extra_threads == 0)
-        {
-            hts_log_debug(__FUNCTION__, "Zero extra threads requested");
-        }
-        else
-        {
-            hts_log_warning(__FUNCTION__, "Invalid negative number of extra threads requested");
-        }
-
-        // read header
+        this.file = HtslibFile(f, mode);
         this.header = header.dup;
-        sam_hdr_write(this.fp,this.header.h);
+        this.file.setHeader(this.header.h);
+
+        if (threads == -1)
+        {
+            hts_log_info(__FUNCTION__,
+                        format("%d CPU cores detected; enabling multithreading", totalCPUs));
+        } else if (threads > totalCPUs)
+            hts_log_warning(__FUNCTION__, "More threads requested than CPU cores detected");
+        else if (threads == 0)
+            hts_log_debug(__FUNCTION__, "Zero threads requested");
+        else
+            hts_log_warning(__FUNCTION__, "Invalid negative number of extra threads requested");
+
+        if (threads > 0 || threads == -1) {
+            this.file.setThreads(threads);
+        }
+
+        this.file.writeHeader;
     }
 
 
     /// Write a SAMRecord to disk
     void write(SAMRecord rec){
-        const auto ret = sam_write1(this.fp, this.header.h, rec.b);
-        assert(ret>=0);
+        this.file.write(rec.b);
     }
 }
 ///
@@ -149,10 +120,10 @@ debug(dhtslib_unittest) unittest
     hts_set_log_level(htsLogLevel.HTS_LOG_TRACE);
     hts_log_info(__FUNCTION__, "Testing SAMWriter");
     hts_log_info(__FUNCTION__, "Loading test file");
-    auto sam = SAMFile(buildPath(dirName(dirName(dirName(dirName(__FILE__)))),"htslib","test","auxf#values.sam"), 0);
+    auto sam = SAMFile(buildPath(dirName(dirName(dirName(dirName(__FILE__)))),"htslib","test","auxf#values.sam"));
     auto sam2 = SAMWriter("/tmp/test.bam",sam.header);
-    auto sam3 = SAMWriter("/tmp/test2.bam",sam.header, SAMWriterTypes.DEDUCE, 4);
-    auto sam4 = SAMWriter("/tmp/test3.bam",sam.header, SAMWriterTypes.DEDUCE, 0);
+    auto sam3 = SAMWriter("/tmp/test2.bam",sam.header, SAMWriterTypes.DEDUCE);
+    auto sam4 = SAMWriter("/tmp/test3.bam",sam.header, SAMWriterTypes.DEDUCE);
 
     hts_log_info(__FUNCTION__, "Getting read 1");
     auto readrange = sam.allRecords;
